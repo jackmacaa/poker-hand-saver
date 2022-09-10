bd5040d3150fa98531b2ef977ef5cbfa4319052a
jackmacaa <jackmaca@msn.com>
2022-09-11 00:32:34 +0930

    added typing to express

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 const express = require('express');
+import { Request, Response } from 'express';
 const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
@@ -6,15 +7,16 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
+    res.sendFile()
     res.sendFile(`${__dirname}/public/index.html`);
 });
 
-app.get("/hand-table", (req, res) => {
+app.get("/hand-table", (req: Request, res: Response) => {
     res.sendFile(`${__dirname}/public/hand-table.html`);
 });
 
-app.get("/hand-save", (req, res) => {
+app.get("/hand-save", (req: Request, res: Response) => {
     res.sendFile(`${__dirname}/public/hand-save.html`);
 });
 
